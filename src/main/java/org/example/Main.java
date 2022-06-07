b34328bab5eddbd6da26a347731a77613c2ce6ec
kajela <titoyut56@gmail.com>
2022-06-07 01:51:17 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,11 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int y = 23;
+        int x = 43;
+        System.out.println(y<=x);
+        System.out.println(45 < 32);
+        System.out.println(10==10);
     }
 }
